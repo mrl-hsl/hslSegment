ff0d8d89ea3fd071b672f5d9266a6efee150b0c7
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-06-04 22:56:41 +0330

    Move data utility functions from convert_to_tfrecords to data_provider

--- data_provider.py
+++ data_provider.py
@@ -15,6 +15,33 @@ input_width = cfg.INPUT_WIDTH
 input_height = cfg.INPUT_HEIGHT
 
 
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def image_example(image_string, label_string):
+    image_shape = tf.image.decode_jpeg(image_string).shape
+    feature = {
+        'height': _int64_feature(image_shape[0]),
+        'width': _int64_feature(image_shape[1]),
+        'depth': _int64_feature(image_shape[2]),
+        'image': _bytes_feature(image_string),
+        'label': _bytes_feature(label_string)
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
 def _parse_image_function(example_proto):
     """
     Create a dictionary describing the features.
