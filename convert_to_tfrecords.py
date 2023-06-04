41b70ac43134cdda8b81e8c17efc8215d7dfe4af
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-06-04 21:13:00 +0330

    Refactor tf record converting script.

--- convert_to_tfrecords.py
+++ convert_to_tfrecords.py
@@ -1,49 +1,74 @@
 import tensorflow as tf
 import cv2
 import glob
+import os
+import numpy as np
+
+# Enable eager execution
 tf.enable_eager_execution()
 
 showSampel = False
 tfRAddress = './'
+writerTrain = tf.python_io.TFRecordWriter(tfRAddress + 'train.tfrecords')
+writerTest = tf.python_io.TFRecordWriter(tfRAddress + 'test.tfrecords')
+src_dir = './data/src_dir/'
+
+
 def _bytes_feature(value):
-  """Returns a bytes_list from a string / byte."""
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
 
 def _float_feature(value):
-  """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def _int64_feature(value):
-  """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-  
-def image_example(image_string, label_string):
-  image_shape = tf.image.decode_jpeg(image_string).shape
-  feature = {
-      'height': _int64_feature(image_shape[0]),
-      'width': _int64_feature(image_shape[1]),
-      'depth': _int64_feature(image_shape[2]),
-      'image': _bytes_feature(image_string),
-      'label': _bytes_feature(label_string)
-  }
-  return tf.train.Example(features=tf.train.Features(feature=feature))
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-writerTrain =tf.python_io.TFRecordWriter(tfRAddress+'train.tfrecords')
-writerTest =tf.python_io.TFRecordWriter(tfRAddress+'test.tfrecords')
-i = 0
-for address in glob.glob('img/*.jpg'):
-  i+=1
-  if len(address.split('_'))==1:
-    image = cv2.imread(address)
-    label = cv2.imread(address.split('.')[0]+'_labeld.jpg')
-    if showSampel==True:
-      cv2.imshow("img",image)
-      cv2.imshow("label",label)
-      cv2.waitKey(0)
-    image_string = cv2.imencode('.jpg', image)[1].tostring()
-    label_string = cv2.imencode('.jpg', label)[1].tostring()
-    tf_example = image_example(image_string, label_string)
-    if i<200:
-      writerTest.write(tf_example.SerializeToString())
-    else:
-      writerTrain.write(tf_example.SerializeToString())
+
+def image_example(image_string, label_string):
+    image_shape = tf.image.decode_jpeg(image_string).shape
+    feature = {
+        'height': _int64_feature(image_shape[0]),
+        'width': _int64_feature(image_shape[1]),
+        'depth': _int64_feature(image_shape[2]),
+        'image': _bytes_feature(image_string),
+        'label': _bytes_feature(label_string)
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+# Count the number of images
+data_counter = 0
+for data_dir in os.listdir(src_dir):
+    data_dir_path = os.path.join(src_dir, data_dir)
+    for img in os.listdir(os.path.join(data_dir_path, 'image')):
+        data_counter = data_counter + 1
+
+cnt = 0 # This counter is used to split the data to 2 parts: train/ and test/
+for data_dir in os.listdir(src_dir):
+    data_dir_path = os.path.join(src_dir, data_dir)
+    for img in os.listdir(os.path.join(data_dir_path, 'image')):
+        cnt += 1
+        img_cv = cv2.imread(os.path.join(data_dir_path, 'image', img))
+        # img_cv = cv2.resize(img_cv, (320, 240),
+        #                     interpolation=cv2.INTER_NEAREST)
+
+        img = img.split('.')[0]
+        label = cv2.imread(os.path.join(data_dir_path, 'label', img + '_labeld.png'))
+        # label = cv2.resize(label, (320, 240), interpolation=cv2.INTER_NEAREST)
+
+        if showSampel == True:
+            cv2.imshow("img", img_cv)
+            cv2.imshow("label", label)
+            cv2.waitKey(0)
+        image_string = cv2.imencode('.png', img_cv)[1].tostring()
+        label_string = cv2.imencode('.png', label)[1].tostring()
+        tf_example = image_example(image_string, label_string)
+        if float(cnt)/float(data_counter) < 0.85:
+            writerTrain.write(tf_example.SerializeToString())
+        else:
+            writerTest.write(tf_example.SerializeToString())
