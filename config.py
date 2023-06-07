4bdb49913d9401d8055f6efa196de9b1ed0494b6
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-06-07 08:42:18 +0330

    Set line label color to white.

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OUTPUT_HEIGHT = 240
 color_palette = {
     'background': np.array([0.0, 0.0, 0.0], dtype=np.float32),  # background
     'grass': np.array([0.0, 255.0, 0.0], dtype=np.float32),  # grass
-    'line': np.array([0.0, 0.0, 255.0], dtype=np.float32),  # line
+    'line': np.array([255.0, 255.0, 255.0], dtype=np.float32),  # line
     # 'ball': [255, 0, 0], # ball
     # 'goal': [255, 0, 0], # goal
     # 'penalty': [0, 255, 255], # penalty
